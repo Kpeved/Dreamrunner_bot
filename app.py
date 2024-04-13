a48935fd08c1dddb47e26fb17fcdac7a58c6aa1b
Mikhail Kulaha <michailkulaga@gmail.com>
2024-04-13 14:45:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ app = Flask(__name__)
 TOKEN = os.getenv("TELEGRAM_TOKEN")
 URL = f"https://api.telegram.org/bot{TOKEN}/"
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Koyeb'
-
 @app.route('/webhook', methods=['POST'])
 def receive_update():
     # Telegram sends updates using POST
@@ -23,6 +19,10 @@ def send_message(chat_id, text):
     url = f"{TELEGRAM_API_URL}/sendMessage"
     data = {'chat_id': chat_id, 'text': text}
     requests.post(url, data=data)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Koyeb'
 
 if __name__ == "__main__":
     app.run()
