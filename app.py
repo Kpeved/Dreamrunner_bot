edcc52c7d67b933bd2e9298a22b9bb208cff6dc8
Mikhail Kulaha <michailkulaga@gmail.com>
2024-04-13 14:04:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,10 @@
-from flask import Flask, request
-import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
+from flask import Flask
 app = Flask(__name__)
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-URL = f"https://api.telegram.org/bot{TOKEN}/"
 
-def send_message(chat_id, text):
-    url = URL + "sendMessage"
-    data = {"chat_id": chat_id, "text": text}
-    requests.post(url, data=data)
+@app.route('/')
+def hello_world():
+    return 'Hello from Koyeb'
 
-@app.route('/<token>', methods=['POST'])
-def respond(token):
-    if token != TOKEN:
-        return "Unauthorized", 403
 
-    update = request.get_json()
-    chat_id = update["message"]["chat"]["id"]
-    text = "Hello Bot"
-    
-    send_message(chat_id, text)
-    
-    return "Success", 200
-
-if __name__ == '__main__':
-    app.run(threaded=True)
+if __name__ == "__main__":
+    app.run()
